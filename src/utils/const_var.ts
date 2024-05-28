db40127fef2bfa719e59699656cfc9169c3b65ad
Luke <heyizhongg@gmail.com>
2024-05-28 16:02:55 +0800

    chore: 建置模擬數據初始化

--- src/utils/const_var.ts
+++ src/utils/const_var.ts
@@ -1,3 +1,29 @@
+export const ApiMethod = {
+  REDIRECT: 'redirect',
+  // HTTP Methods
+  POST: 'post',
+  GET: 'get',
+  PATCH: 'patch',
+  DELETE: 'delete',
+  PUT: 'put',
+  // 分頁預設值
+  DEFAULT_PAGE_NUMBER: 1,
+  DEFAULT_PAGE_SIZE: 10,
+  // 模擬響應延遲
+  SIMULATION_DELAY_MS: 250,
+  // Standard HTTP Success Code
+  HTTP_OK: 200,
+  // User Relationship Status
+  RelationshipStatus: {
+    CANNOT_UNFOLLOW_RECOMMENDED: -2, // 推薦,不能移除
+    RECOMMENDED: -1, // 推薦用戶
+    FOLLOW_ME: 1, // 只追蹤我
+    MUTUAL_FOLLOW: 2, // 相互追蹤
+    I_FOLLOW: 3, // 我追蹤他
+    FOLLOW_REQUEST: 4 // 待處理的關注請求
+  }
+}
+
 export const SlideType = {
   HORIZONTAL: 0,
   VERTICAL: 1,
